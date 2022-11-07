cc1389f10d1d3bd3dd27f16d37dc64246e9d50bb
Mandy Raasch <mandy.raasch@gmail.com>
2022-11-07 17:51:40 +0100

    Header AppBar added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,34 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 const Header = () => {
   return (
     <div>
-      header
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Energiespar-Tipps
+          </Typography>
+          <Button color="inherit" href='/register'>Register</Button>
+          <Button color="inherit" href='/login'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </div>
   )
 }
