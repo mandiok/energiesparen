edf450444702781ba284a7a8b68c600375a14b62
Katharina <107190551+katarguedas@users.noreply.github.com>
2022-11-09 15:44:09 +0100

    add InputPost component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,17 @@ import Header from './components/Header';
 import  Login from './components/Login';
 import Register from './components/Register';
 import Post from './components/Post';
+import InputPost from './components/InputPost';
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Register />
-      <Login />
-      <Post />
+      {/* <Header /> */}
+      {/* <Register /> */}
+      {/* <Login /> */}
+      <InputPost />
+      {/* <Post /> */}
     </div>
     
   );
