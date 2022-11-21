1379473f0b859ecacb811a69acd9ae37fd623a08
Katharina <107190551+katarguedas@users.noreply.github.com>
2022-11-21 20:12:08 +0100

    contextProvider Part 1

--- frontend/src/components/Feed.js
+++ frontend/src/components/Feed.js
@@ -1,8 +1,9 @@
 import Post from "./Post";
 import { Container, Box } from "@mui/material";
-import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
+import { postContext } from "../App";
 
+import { useContext } from "react";
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
@@ -17,128 +18,130 @@ const Feed = () => {
 
     const [selection, setSelection] = useState('Datum');
 
+    const {posts} = useContext(postContext)
 
+    console.log("posts:", posts)
     const handleChange = (event) => {
         setSelection(event.target.value);
     }
 
-    const postArray = [{
-        id: "01",
-        userId: "asdfasdgfa",
-        date: "23. Oktober 2022",
-        title: "Wäschetrocknen in der Sonne",
-        text: "Heute war wieder ein perfekter Tag, um die Wäsche draußen zu trocknen. Wer eine Terrasse, einen Garten oder einen Balkon hat, braucht keinen Trockner!",
-        link: "",
-        picture: "??",
-        likes: ["4626-457484-99999", "12341-57895-24521-15415"],
-        comments: [
-            {
-                id: uuidv4(),
-                userId: "usernummereins",
-                date: "datum",
-                text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-            },
-            {
-                id: uuidv4(),
-                userId: "usernummerdreis",
-                date: "datum",
-                text: "tua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-            }
-        ]
-    },
-    {
-        id: "02",
-        userId: uuidv4(),
-        date: "26. Oktober 2022",
-        title: "Smartphone ausschalten",
-        text: "Hey, ich habe es heute geschafft, mein Smartphone für 3 Stunden ausgeschaltet zu lassen! :-) Fühlte sich komisch an, aber ich gabe es geschafft.",
-        link: "",
-        picture: "??",
-        likes: ["4626-457484-24526", "12341-57895-24521-15415", "cdb63720-9628-5ef6-bbca-2e094f3c", "cd20-9628-5ef6-bbca-e6094f3c"],
-        comments: [
-            {
-                id: uuidv4(),
-                userId: "usernummer2",
-                date: "datum",
-                text: "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
-            }
-        ]
-    },
-    {
-        id: "03",
-        userId: uuidv4(),
-        date: "01. November 2022",
-        title: " T-Shirt",
-        text: "sequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue ",
-        link: "",
-        picture: "??",
-        likes: ["4626-457484-24526"],
-        comments: []
-    },
-    {
-        id: "04",
-        userId: uuidv4(),
-        date: "06. November 2022",
-        title: "Pullover statt T-Shirt",
-        text: "dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue, ril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet ",
-        link: "",
-        picture: "??",
-        likes: ["4626-457484-24526", "2354-2345265-246267"],
-        comments: [
-            {
-                id: uuidv4(),
-                userId: "usernummer3",
-                date: "datum",
-                text: "Duiait nulla facilisi. Lorem ipsum dolor sit amet,"
-            }
-        ]
-    },
-    {
-        id: "05",
-        userId: uuidv4(),
-        date: "09. November 2022",
-        title: "19 Grad Celcius",
-        text: "dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue, ril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit pra",
-        link: "",
-        picture: "??",
-        likes: ["4626-457484-24526", "2354-2345265-246267", "cd20-9628-5ef6-bbca-e6094f3c"],
-        comments: []
-    },
-    {
-        id: "06",
-        userId: uuidv4(),
-        date: "10. November 2022",
-        title: "Dummy Text",
-        text: " odio dignissim qui blandit praesent luptatum zzril delenit augue, ril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit pra",
-        link: "",
-        picture: "??",
-        likes: ["2354-2345265-246267", "cd20-9628-5ef6-bbca-e6094f3c"],
-        comments: []
-    },
-    {
-        id: "07",
-        userId: uuidv4(),
-        date: "12. November 2022",
-        title: "Smartphones",
-        text: "Ich habe einen interessanten Artikel gelesen, wie viel Strom wir täglich mit unseren Smartphones verbrauchen. Wirklich guter Artikel, den ich Euch hier weiter empfehle..... ",
-        link: "",
-        picture: "??",
-        likes: ["4626-457484-24526", "12341-57895-24521-15415", "cd20-9628-5ef6-bbca-e6094f3c"],
-        comments: [
-            {
-                id: uuidv4(),
-                userId: "usernummer24",
-                date: "datum",
-                text: "Duisolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
-            }
-        ]
-    }
-    ]
+    // const postArray = [{
+    //     id: "01",
+    //     userId: "asdfasdgfa",
+    //     date: "23. Oktober 2022",
+    //     title: "Wäschetrocknen in der Sonne",
+    //     text: "Heute war wieder ein perfekter Tag, um die Wäsche draußen zu trocknen. Wer eine Terrasse, einen Garten oder einen Balkon hat, braucht keinen Trockner!",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["4626-457484-99999", "12341-57895-24521-15415"],
+    //     comments: [
+    //         {
+    //             id: uuidv4(),
+    //             userId: "usernummereins",
+    //             date: "datum",
+    //             text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    //         },
+    //         {
+    //             id: uuidv4(),
+    //             userId: "usernummerdreis",
+    //             date: "datum",
+    //             text: "tua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    //         }
+    //     ]
+    // },
+    // {
+    //     id: "02",
+    //     userId: uuidv4(),
+    //     date: "26. Oktober 2022",
+    //     title: "Smartphone ausschalten",
+    //     text: "Hey, ich habe es heute geschafft, mein Smartphone für 3 Stunden ausgeschaltet zu lassen! :-) Fühlte sich komisch an, aber ich gabe es geschafft.",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["4626-457484-24526", "12341-57895-24521-15415", "cdb63720-9628-5ef6-bbca-2e094f3c", "cd20-9628-5ef6-bbca-e6094f3c"],
+    //     comments: [
+    //         {
+    //             id: uuidv4(),
+    //             userId: "usernummer2",
+    //             date: "datum",
+    //             text: "Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+    //         }
+    //     ]
+    // },
+    // {
+    //     id: "03",
+    //     userId: uuidv4(),
+    //     date: "01. November 2022",
+    //     title: " T-Shirt",
+    //     text: "sequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue ",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["4626-457484-24526"],
+    //     comments: []
+    // },
+    // {
+    //     id: "04",
+    //     userId: uuidv4(),
+    //     date: "06. November 2022",
+    //     title: "Pullover statt T-Shirt",
+    //     text: "dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue, ril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet ",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["4626-457484-24526", "2354-2345265-246267"],
+    //     comments: [
+    //         {
+    //             id: uuidv4(),
+    //             userId: "usernummer3",
+    //             date: "datum",
+    //             text: "Duiait nulla facilisi. Lorem ipsum dolor sit amet,"
+    //         }
+    //     ]
+    // },
+    // {
+    //     id: "05",
+    //     userId: uuidv4(),
+    //     date: "09. November 2022",
+    //     title: "19 Grad Celcius",
+    //     text: "dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue, ril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit pra",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["4626-457484-24526", "2354-2345265-246267", "cd20-9628-5ef6-bbca-e6094f3c"],
+    //     comments: []
+    // },
+    // {
+    //     id: "06",
+    //     userId: uuidv4(),
+    //     date: "10. November 2022",
+    //     title: "Dummy Text",
+    //     text: " odio dignissim qui blandit praesent luptatum zzril delenit augue, ril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit pra",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["2354-2345265-246267", "cd20-9628-5ef6-bbca-e6094f3c"],
+    //     comments: []
+    // },
+    // {
+    //     id: "07",
+    //     userId: uuidv4(),
+    //     date: "12. November 2022",
+    //     title: "Smartphones",
+    //     text: "Ich habe einen interessanten Artikel gelesen, wie viel Strom wir täglich mit unseren Smartphones verbrauchen. Wirklich guter Artikel, den ich Euch hier weiter empfehle..... ",
+    //     link: "",
+    //     picture: "??",
+    //     likes: ["4626-457484-24526", "12341-57895-24521-15415", "cd20-9628-5ef6-bbca-e6094f3c"],
+    //     comments: [
+    //         {
+    //             id: uuidv4(),
+    //             userId: "usernummer24",
+    //             date: "datum",
+    //             text: "Duisolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet,"
+    //         }
+    //     ]
+    // }
+    // ]
 
     const steps = 2
 
-    const [posts, setPosts] = useState(postArray);
-    const [indexStart, setIndexStart] = useState(1);
+    // const [posts, setPosts] = useState(postArray);
+    const [indexStart, setIndexStart] = useState(0);
     const [indexEnd, setIndexEnd] = useState(indexStart + steps);
 
     //----------
@@ -226,12 +229,10 @@ const Feed = () => {
 
                         <Post
                             key={e.id}
-                            posts={posts}
-                            setPosts={setPosts}
                             post={e}
-                            userName={"testuser"}
-                            userId={"2345-23452"}
-                            firstName={"Max"}
+                            userName={e.user_name}
+                            userId={e.id}
+                            firstName={e.first_name}
                         />
                     ))
                 }
---
+++ frontend/src/hooks/usePosts.js
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+
+    const usePosts = () => {
+        const [posts, setPosts] = useState(
+            [{
+                id: "",
+                userId: "",
+                date: "",
+                title: "",
+                text: "",
+                link: "",
+                picture: "",
+                likes: [],
+                comments: []
+            }]
+        );
+
+    
+//Post hinzufügen
+const addPost = (newPost) => {
+
+    setPosts([...posts, newPost]);
+    addPostsToBackend(posts);
+}
+
+//Backend Routen
+const addPostsToBackend = (posts) => {
+    fetch("http://localhost:3001/posts", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(posts),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+};
+
+
+const getPostsFromBackend = () => {
+    fetch("http://localhost:3001/posts")
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Success:", data);   
+            setPosts(data.result)             
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+};
+
+
+//Posts laden
+useEffect(() => {
+    getPostsFromBackend();
+}, []);
+
+return [posts, setPosts, addPost];
+};
+
+export default usePosts;
+
+
+
+//BACKUP:
+//Handler aus Frontend???
+    //const [selection, setSelection] = useState("Datum");
+
+  
+
+    // const sort = (sortSelection) => {
+    //     if (sortSelection === "Likes") {
+    //         let array = [];
+    //         for (let i = 0; i < posts.length; i++) {
+    //             array.push([posts[i].likes.length, posts[i].id]);
+    //         }
+
+    //         array.sort(function (a, b) {
+    //             return a[0] - b[0]
+    //         })
+
+    //         array.reverse();
+
+    //         let postsorted = [];
+    //         for (let k = 0; k < posts.length; k++) {
+    //             for (let n = 0; n < array.length; n++) {
+    //                 {
+    //                     if (array[k][1] === posts[n].id) {
+    //                         postsorted.push(posts[n]);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //         return postsorted
+    //     }
+    //     else if (sortSelection === "Datum") {
+    //         return(posts)
+    //     }
+    // }
