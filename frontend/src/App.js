efd911bed033f4418567ce1b4a2c24070938f233
Katharina <107190551+katarguedas@users.noreply.github.com>
2022-12-01 21:42:01 +0100

    Token Expiration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,29 @@
-import Header from './components/Header';
 import Login from './components/Login';
 import Register from './components/Register';
 import Mainpage from './components/Mainpage';
 import Profile from './components/Profile';
-import usePosts from './hooks/usePosts';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useContext } from 'react';
 import { AppContext } from './providers/AppContext';
+import AuthVerify from './components/AuthVerify';
 
 function App() {
 
-  const { user, posts } = useContext(AppContext);
+  const { user } = useContext(AppContext);
+
 
   return (
-    <div className="App">  
-        <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
         <Routes>
-          <Route path='/' element={ user ? <Mainpage /> : <Login />} />
-          <Route path='/login' element={ <Login />} />
-          <Route path='/register' element={ <Register /> } />
-          <Route path='/profile' element={ <Profile />} />
+          <Route path='/' element={user ? <Mainpage /> : <Login />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
 
-        </BrowserRouter>
-
-
+        <AuthVerify />
+      </BrowserRouter>
     </div>
 
   );
---
+++ frontend/src/components/AuthVerify.js
@@ -0,0 +1,68 @@
+import { useEffect } from "react";
+import { useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { AppContext } from '../providers/AppContext';
+import jwt_decode from "jwt-decode";
+
+//-----------------------------------------------
+
+
+const AuthVerify = () => {
+
+  const { LOCAL_STORAGE_KEY, user, logoutUser, setToken, setUserData, setUser } = useContext(AppContext);
+
+  let location = useLocation();
+
+  //..........................
+  const verifyToken = async (user) => {
+
+    const response = await fetch('/refreshtoken', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ user }),
+    });
+    const data = await response.json();
+    if (data.status === "ok") {
+      alert("Login successful");
+
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data))
+
+      setToken(data.access)
+      var decodedJwt = jwt_decode(data.access);
+      setUserData(decodedJwt);
+      setUser(decodedJwt.email)
+
+      // console.log("token:", data.access)
+      // console.log('userData:', decodedJwt)
+      // console.log("user:", decodedJwt.email)
+    } else {
+      alert("Token refresh failed");
+      logoutUser()
+    }
+  };
+
+  //......................
+
+  useEffect(() => {
+    const ls = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+    if (ls) {
+      const decodedJwt = jwt_decode(ls.access)
+
+      console.log("Zeitdifferenz:", (decodedJwt.exp * 1000) - Date.now())
+
+      if (decodedJwt.exp * 1000 < Date.now()) {
+        console.log("Token abgelaufen, frage nach einem neuen")
+        verifyToken(user);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
+  return;
+};
+
+
+export default AuthVerify;
